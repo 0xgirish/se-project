e1967b938ce9e6e3b3692ba609d3af3ef5231434
divjot0 <33392716+divjot0@users.noreply.github.com>
2019-04-17 03:56:47 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from barcodelookup.models import Product
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
 
 # Create your views here.
 def home_page(request):
@@ -9,9 +10,24 @@ def home_page(request):
     }
     return render(request, "home.html", context)
 
+
 def location(request):
 	return render(request, "location.html")
 
 def register(request):
+	if request.method=='POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			return redirect("/")
+		else:
+			for msg in form.error_messages:
+				print(form.error_messages[msg])
+			return render(request = request,
+                          template_name = "register.html",
+                          context={"form":form})
 	form = UserCreationForm
-	return render(request = request, template_name = "register.html", context={"form":form})
+	return render(request = request,
+                  template_name = "register.html",
+                  context={"form":form})
