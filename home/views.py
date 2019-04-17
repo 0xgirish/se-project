e13e6827b68af7685cf07931bda765109d62fb62
divjot0 <33392716+divjot0@users.noreply.github.com>
2019-04-17 17:58:23 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from barcodelookup.models import Product
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 # Create your views here.
 def home_page(request):
@@ -19,11 +20,13 @@ def register(request):
 		form = UserCreationForm(request.POST)
 		if form.is_valid():
 			user = form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f"New account created: {username}")
 			login(request, user)
 			return redirect("/")
 		else:
 			for msg in form.error_messages:
-				print(form.error_messages[msg])
+				messages.error(request, f"{msg}: {form.error_messages[msg]}")
 			return render(request = request,
                           template_name = "register.html",
                           context={"form":form})
@@ -35,3 +38,23 @@ def register(request):
 def logout_request(request):
 	logout(request)
 	return redirect("/")
+
+def login_request(request):
+	if request.method == 'POST':
+		form = AuthenticationForm(request=request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}")
+				return redirect('/')
+			else:
+				messages.error(request, "Invalid username or password.")
+		else:
+			messages.error(request, "Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request = request,
+                  template_name = "login.html",
+                  context={"form":form})
