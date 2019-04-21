7a9f9798e74b287868e2535e6714b5ca9e5ca528
divjot0 <33392716+divjot0@users.noreply.github.com>
2019-04-21 16:12:57 +0530

    Update admin.py

--- barcodelookup/admin.py
+++ barcodelookup/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, Book
+from .models import Product, Book, UserProfile
 # Register your models here.
 
 # register Product database for admin panel
 admin.site.register(Product)
 admin.site.register(Book)
+admin.site.register(UserProfile)
